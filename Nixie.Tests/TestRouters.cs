cf5440e3d15fd3c736a5c2fc754d3c9f688fb909
Andres Gutierrez <gutierrezandresfelipe@gmail.com>
2023-10-17 20:38:56 -0500

    Added extensions to create routers in a friendly way

--- Nixie.Tests/TestRouters.cs
+++ Nixie.Tests/TestRouters.cs
@@ -1,7 +1,6 @@
 ﻿
 using Nixie.Routers;
 using Nixie.Tests.Actors;
-using System.Diagnostics.Metrics;
 
 namespace Nixie.Tests;
 
@@ -15,6 +14,67 @@ public sealed class TestRouters
 
         IActorRef<RoundRobinActor<RouteeActor, RouterMessage>, RouterMessage> router =
             asx.Spawn<RoundRobinActor<RouteeActor, RouterMessage>, RouterMessage>("my-router", 5);
+
+        router.Send(new RouterMessage(RouterMessageType.Route, "aaa"));
+        router.Send(new RouterMessage(RouterMessageType.Route, "bbb"));
+        router.Send(new RouterMessage(RouterMessageType.Route, "ccc"));
+        router.Send(new RouterMessage(RouterMessageType.Route, "ddd"));
+        router.Send(new RouterMessage(RouterMessageType.Route, "eee"));
+
+        await asx.Wait();
+
+        Assert.IsAssignableFrom<RoundRobinActor<RouteeActor, RouterMessage>>(router.Runner.Actor);
+
+        RoundRobinActor<RouteeActor, RouterMessage> routerActor = (RoundRobinActor<RouteeActor, RouterMessage>)router.Runner.Actor!;
+
+        foreach (IActorRef<RouteeActor, RouterMessage> routee in routerActor.Instances)
+        {
+            Assert.IsAssignableFrom<RouteeActor>(routee.Runner.Actor);
+
+            RouteeActor routeeActor = (RouteeActor)routee.Runner.Actor!;
+            Assert.Equal(1, routeeActor.GetMessages());
+        }
+    }
+
+    [Fact]
+    public async void TestCreateRoundRobinRouterExt()
+    {
+        using ActorSystem asx = new();
+
+        IActorRef<RoundRobinActor<RouteeActor, RouterMessage>, RouterMessage> router = asx.CreateRoundRobinRouter<RouteeActor, RouterMessage>("my-router", 5);
+
+        router.Send(new RouterMessage(RouterMessageType.Route, "aaa"));
+        router.Send(new RouterMessage(RouterMessageType.Route, "bbb"));
+        router.Send(new RouterMessage(RouterMessageType.Route, "ccc"));
+        router.Send(new RouterMessage(RouterMessageType.Route, "ddd"));
+        router.Send(new RouterMessage(RouterMessageType.Route, "eee"));
+
+        await asx.Wait();
+
+        Assert.IsAssignableFrom<RoundRobinActor<RouteeActor, RouterMessage>>(router.Runner.Actor);
+
+        RoundRobinActor<RouteeActor, RouterMessage> routerActor = (RoundRobinActor<RouteeActor, RouterMessage>)router.Runner.Actor!;
+
+        foreach (IActorRef<RouteeActor, RouterMessage> routee in routerActor.Instances)
+        {
+            Assert.IsAssignableFrom<RouteeActor>(routee.Runner.Actor);
+
+            RouteeActor routeeActor = (RouteeActor)routee.Runner.Actor!;
+            Assert.Equal(1, routeeActor.GetMessages());
+        }
+    }
+
+    [Fact]
+    public async void TestCreateRoundRobinRouterExtInstances()
+    {
+        using ActorSystem asx = new();
+
+        List<IActorRef<RouteeActor, RouterMessage>> instances = new();
+
+        for (int i = 0; i < 5; i++)
+            instances.Add(asx.Spawn<RouteeActor, RouterMessage>());
+
+        IActorRef<RoundRobinActor<RouteeActor, RouterMessage>, RouterMessage> router = asx.CreateRoundRobinRouter("my-router", instances);
 
         router.Send(new RouterMessage(RouterMessageType.Route, "aaa"));
         router.Send(new RouterMessage(RouterMessageType.Route, "bbb"));
@@ -91,12 +151,105 @@ public sealed class TestRouters
     }
 
     [Fact]
+    public async void TestCreateRoundRobinRouterReplyExtInstances()
+    {
+        using ActorSystem asx = new();
+
+        List<IActorRef<RouteeReplyActor, RouterMessage, RouterResponse>> instances = new();
+
+        for (int i = 0; i < 5; i++)
+            instances.Add(asx.Spawn<RouteeReplyActor, RouterMessage, RouterResponse>());
+
+        IActorRef<RoundRobinActor<RouteeReplyActor, RouterMessage, RouterResponse>, RouterMessage, RouterResponse> router =
+            asx.CreateRoundRobinRouter("my-router", instances);
+
+        await router.Ask(new RouterMessage(RouterMessageType.Route, "aaa"));
+        await router.Ask(new RouterMessage(RouterMessageType.Route, "bbb"));
+        await router.Ask(new RouterMessage(RouterMessageType.Route, "ccc"));
+        await router.Ask(new RouterMessage(RouterMessageType.Route, "ddd"));
+        await router.Ask(new RouterMessage(RouterMessageType.Route, "eee"));
+
+        Assert.IsAssignableFrom<RoundRobinActor<RouteeReplyActor, RouterMessage, RouterResponse>>(router.Runner.Actor);
+
+        RoundRobinActor<RouteeReplyActor, RouterMessage, RouterResponse> routerActor = (RoundRobinActor<RouteeReplyActor, RouterMessage, RouterResponse>)router.Runner.Actor!;
+
+        foreach (IActorRef<RouteeReplyActor, RouterMessage, RouterResponse> routee in routerActor.Instances)
+        {
+            Assert.IsAssignableFrom<RouteeReplyActor>(routee.Runner.Actor);
+
+            RouteeReplyActor routeeActor = (RouteeReplyActor)routee.Runner.Actor!;
+            Assert.Equal(1, routeeActor.GetMessages());
+        }
+    }
+
+    [Fact]
+    public async void TestCreateRoundRobinRouterReplyExt()
+    {
+        using ActorSystem asx = new();
+
+        IActorRef<RoundRobinActor<RouteeReplyActor, RouterMessage, RouterResponse>, RouterMessage, RouterResponse> router =
+            asx.CreateRoundRobinRouter<RouteeReplyActor, RouterMessage, RouterResponse>("my-router", 5);
+
+        await router.Ask(new RouterMessage(RouterMessageType.Route, "aaa"));
+        await router.Ask(new RouterMessage(RouterMessageType.Route, "bbb"));
+        await router.Ask(new RouterMessage(RouterMessageType.Route, "ccc"));
+        await router.Ask(new RouterMessage(RouterMessageType.Route, "ddd"));
+        await router.Ask(new RouterMessage(RouterMessageType.Route, "eee"));
+
+        Assert.IsAssignableFrom<RoundRobinActor<RouteeReplyActor, RouterMessage, RouterResponse>>(router.Runner.Actor);
+
+        RoundRobinActor<RouteeReplyActor, RouterMessage, RouterResponse> routerActor = (RoundRobinActor<RouteeReplyActor, RouterMessage, RouterResponse>)router.Runner.Actor!;
+
+        foreach (IActorRef<RouteeReplyActor, RouterMessage, RouterResponse> routee in routerActor.Instances)
+        {
+            Assert.IsAssignableFrom<RouteeReplyActor>(routee.Runner.Actor);
+
+            RouteeReplyActor routeeActor = (RouteeReplyActor)routee.Runner.Actor!;
+            Assert.Equal(1, routeeActor.GetMessages());
+        }
+    }
+
+    [Fact]
     public async void TestCreateConsistentHashRouter()
     {
         using ActorSystem asx = new();
 
         IActorRef<ConsistentHashActor<RouteeActor, RouterMessage>, RouterMessage> router =
             asx.Spawn<ConsistentHashActor<RouteeActor, RouterMessage>, RouterMessage>("my-router", 5);
+
+        router.Send(new RouterMessage(RouterMessageType.Route, "aaa"));
+        router.Send(new RouterMessage(RouterMessageType.Route, "bbb"));
+        router.Send(new RouterMessage(RouterMessageType.Route, "ccc"));
+        router.Send(new RouterMessage(RouterMessageType.Route, "ddd"));
+        router.Send(new RouterMessage(RouterMessageType.Route, "eee"));
+
+        await asx.Wait();
+
+        Assert.IsAssignableFrom<ConsistentHashActor<RouteeActor, RouterMessage>>(router.Runner.Actor);
+
+        ConsistentHashActor<RouteeActor, RouterMessage> routerActor = (ConsistentHashActor<RouteeActor, RouterMessage>)router.Runner.Actor!;
+
+        foreach (IActorRef<RouteeActor, RouterMessage> routee in routerActor.Instances)
+        {
+            Assert.IsAssignableFrom<RouteeActor>(routee.Runner.Actor);
+
+            RouteeActor routeeActor = (RouteeActor)routee.Runner.Actor!;
+            Assert.Equal(1, routeeActor.GetMessages());
+        }
+    }
+
+    [Fact]
+    public async void TestCreateConsistentHashRouterExt()
+    {
+        using ActorSystem asx = new();
+
+        List<IActorRef<RouteeActor, RouterMessage>> instances = new();
+
+        for (int i = 0; i < 5; i++)
+            instances.Add(asx.Spawn<RouteeActor, RouterMessage>());
+
+        IActorRef<ConsistentHashActor<RouteeActor, RouterMessage>, RouterMessage> router =
+            asx.Spawn<ConsistentHashActor<RouteeActor, RouterMessage>, RouterMessage>("my-router", instances);
 
         router.Send(new RouterMessage(RouterMessageType.Route, "aaa"));
         router.Send(new RouterMessage(RouterMessageType.Route, "bbb"));
---
+++ Nixie/Routers/ActorSystemExtensions.cs
@@ -0,0 +1,312 @@
+﻿
+namespace Nixie.Routers;
+
+/// <summary>
+/// Extensions to create routers in a friendly way
+/// </summary>
+public static class ActorSystemExtensions
+{
+    /// <summary>
+    /// Creates a Round-Robin router specifying name and number of instances
+    /// </summary>
+    /// <typeparam name="TActor"></typeparam>
+    /// <typeparam name="TRequest"></typeparam>
+    /// <param name="actorSystem"></param>
+    /// <param name="name"></param>
+    /// <param name="instances"></param>
+    /// <returns></returns>
+    public static IActorRef<RoundRobinActor<TActor, TRequest>, TRequest> CreateRoundRobinRouter<TActor, TRequest>(
+        this ActorSystem actorSystem,
+        string name,
+        int instances
+    )
+        where TActor : IActor<TRequest> where TRequest : class
+    {        
+         return actorSystem.Spawn<RoundRobinActor<TActor, TRequest>, TRequest>(name, instances);
+    }
+
+    /// <summary>
+    /// Creates a Round-Robin router specifying number of instances
+    /// </summary>
+    /// <typeparam name="TActor"></typeparam>
+    /// <typeparam name="TRequest"></typeparam>
+    /// <param name="actorSystem"></param>
+    /// <param name="instances"></param>
+    /// <returns></returns>
+    public static IActorRef<RoundRobinActor<TActor, TRequest>, TRequest> CreateRoundRobinRouter<TActor, TRequest>(
+        this ActorSystem actorSystem,        
+        int instances
+    )
+        where TActor : IActor<TRequest> where TRequest : class
+    {
+        return actorSystem.Spawn<RoundRobinActor<TActor, TRequest>, TRequest>(null, instances);
+    }
+
+    /// <summary>
+    /// Creates a Round-Robin router specifying name and a list of routee actors
+    /// </summary>
+    /// <typeparam name="TActor"></typeparam>
+    /// <typeparam name="TRequest"></typeparam>
+    /// <param name="actorSystem"></param>
+    /// <param name="name"></param>
+    /// <param name="instances"></param>
+    /// <returns></returns>
+    public static IActorRef<RoundRobinActor<TActor, TRequest>, TRequest> CreateRoundRobinRouter<TActor, TRequest>(
+        this ActorSystem actorSystem,
+        string name,
+        List<IActorRef<TActor, TRequest>> instances
+    )
+        where TActor : IActor<TRequest> where TRequest : class
+    {
+        return actorSystem.Spawn<RoundRobinActor<TActor, TRequest>, TRequest>(name, instances);
+    }
+
+    /// <summary>
+    /// Creates a Round-Robin router specifying a list of routee actors7uh
+    /// </summary>
+    /// <typeparam name="TActor"></typeparam>
+    /// <typeparam name="TRequest"></typeparam>
+    /// <param name="actorSystem"></param>
+    /// <param name="instances"></param>
+    /// <returns></returns>
+    public static IActorRef<RoundRobinActor<TActor, TRequest>, TRequest> CreateRoundRobinRouter<TActor, TRequest>(
+        this ActorSystem actorSystem,
+        List<IActorRef<TActor, TRequest>> instances
+    )
+        where TActor : IActor<TRequest> where TRequest : class
+    {
+        return actorSystem.Spawn<RoundRobinActor<TActor, TRequest>, TRequest>(null, instances);
+    }
+
+    /// <summary>
+    /// Creates a Round-Robin router specifying name and number of instances
+    /// </summary>
+    /// <typeparam name="TActor"></typeparam>
+    /// <typeparam name="TRequest"></typeparam>
+    /// <typeparam name="TResponse"></typeparam>
+    /// <param name="actorSystem"></param>
+    /// <param name="name"></param>
+    /// <param name="instances"></param>
+    /// <returns></returns>
+    public static IActorRef<RoundRobinActor<TActor, TRequest, TResponse>, TRequest, TResponse>
+        CreateRoundRobinRouter<TActor, TRequest, TResponse>(
+            this ActorSystem actorSystem,
+            string name,
+            int instances
+        )
+        where TActor : IActor<TRequest, TResponse> where TRequest : class where TResponse : class
+    {
+        return actorSystem.Spawn<RoundRobinActor<TActor, TRequest, TResponse>, TRequest, TResponse>(name, instances);
+    }
+
+    /// <summary>
+    /// Creates a Round-Robin router specifying name and number of instances
+    /// </summary>
+    /// <typeparam name="TActor"></typeparam>
+    /// <typeparam name="TRequest"></typeparam>
+    /// <typeparam name="TResponse"></typeparam>
+    /// <param name="actorSystem"></param>
+    /// <param name="instances"></param>
+    /// <returns></returns>
+    public static IActorRef<RoundRobinActor<TActor, TRequest, TResponse>, TRequest, TResponse>
+        CreateRoundRobinRouter<TActor, TRequest, TResponse>(
+            this ActorSystem actorSystem,
+            int instances
+        )
+        where TActor : IActor<TRequest, TResponse> where TRequest : class where TResponse : class
+    {
+        return actorSystem.Spawn<RoundRobinActor<TActor, TRequest, TResponse>, TRequest, TResponse>(null, instances);
+    }
+
+    /// <summary>
+    /// Creates a Round-Robin router specifying name and a list of instances
+    /// </summary>
+    /// <typeparam name="TActor"></typeparam>
+    /// <typeparam name="TRequest"></typeparam>
+    /// <typeparam name="TResponse"></typeparam>
+    /// <param name="actorSystem"></param>
+    /// <param name="name"></param>
+    /// <param name="instances"></param>
+    /// <returns></returns>
+    public static IActorRef<RoundRobinActor<TActor, TRequest, TResponse>, TRequest, TResponse>
+        CreateRoundRobinRouter<TActor, TRequest, TResponse>(
+            this ActorSystem actorSystem,
+            string name,
+            List<IActorRef<TActor, TRequest, TResponse>> instances
+        )
+        where TActor : IActor<TRequest, TResponse> where TRequest : class where TResponse : class
+    {
+        return actorSystem.Spawn<RoundRobinActor<TActor, TRequest, TResponse>, TRequest, TResponse>(name, instances);
+    }
+
+    /// <summary>
+    /// Creates a Round-Robin router specifying name and a list of instances
+    /// </summary>
+    /// <typeparam name="TActor"></typeparam>
+    /// <typeparam name="TRequest"></typeparam>
+    /// <typeparam name="TResponse"></typeparam>
+    /// <param name="actorSystem"></param>
+    /// <param name="instances"></param>
+    /// <returns></returns>
+    public static IActorRef<RoundRobinActor<TActor, TRequest, TResponse>, TRequest, TResponse>
+        CreateRoundRobinRouter<TActor, TRequest, TResponse>(
+            this ActorSystem actorSystem,
+            List<IActorRef<TActor, TRequest, TResponse>> instances
+        )
+        where TActor : IActor<TRequest, TResponse> where TRequest : class where TResponse : class
+    {
+        return actorSystem.Spawn<RoundRobinActor<TActor, TRequest, TResponse>, TRequest, TResponse>(null, instances);
+    }
+
+    /// <summary>
+    /// Creates a Round-Robin router specifying number of instances
+    /// </summary>
+    /// <typeparam name="TActor"></typeparam>
+    /// <typeparam name="TRequest"></typeparam>
+    /// <param name="actorSystem"></param>
+    /// <param name="name"></param>
+    /// <param name="instances"></param>
+    /// <returns></returns>
+    public static IActorRef<ConsistentHashActor<TActor, TRequest>, TRequest> CreateConsistentHashRouter<TActor, TRequest>(
+        this ActorSystem actorSystem,
+        string name,
+        int instances
+    )
+        where TActor : IActor<TRequest> where TRequest : class, IConsistentHashable
+    {
+        return actorSystem.Spawn<ConsistentHashActor<TActor, TRequest>, TRequest>(name, instances);
+    }
+
+    /// <summary>
+    /// Creates a Consistent Hash router specifying name and number of instances
+    /// </summary>
+    /// <typeparam name="TActor"></typeparam>
+    /// <typeparam name="TRequest"></typeparam>
+    /// <param name="actorSystem"></param>
+    /// <param name="instances"></param>
+    /// <returns></returns>
+    public static IActorRef<ConsistentHashActor<TActor, TRequest>, TRequest> CreateConsistentHashRouter<TActor, TRequest>(
+        this ActorSystem actorSystem,
+        int instances
+    )
+        where TActor : IActor<TRequest> where TRequest : class, IConsistentHashable
+    {
+        return actorSystem.Spawn<ConsistentHashActor<TActor, TRequest>, TRequest>(null, instances);
+    }
+
+    /// <summary>
+    /// Creates a Round-Robin router specifying a list of instances
+    /// </summary>
+    /// <typeparam name="TActor"></typeparam>
+    /// <typeparam name="TRequest"></typeparam>
+    /// <param name="actorSystem"></param>
+    /// <param name="name"></param>
+    /// <param name="instances"></param>
+    /// <returns></returns>
+    public static IActorRef<ConsistentHashActor<TActor, TRequest>, TRequest> CreateConsistentHashRouter<TActor, TRequest>(
+        this ActorSystem actorSystem,
+        string name,
+        List<IActorRef<TActor, TRequest>> instances
+    )
+        where TActor : IActor<TRequest> where TRequest : class, IConsistentHashable
+    {
+        return actorSystem.Spawn<ConsistentHashActor<TActor, TRequest>, TRequest>(name, instances);
+    }
+
+    /// <summary>
+    /// Creates a Consistent Hash router specifying name and a list of instances
+    /// </summary>
+    /// <typeparam name="TActor"></typeparam>
+    /// <typeparam name="TRequest"></typeparam>
+    /// <param name="actorSystem"></param>
+    /// <param name="instances"></param>
+    /// <returns></returns>
+    public static IActorRef<ConsistentHashActor<TActor, TRequest>, TRequest> CreateConsistentHashRouter<TActor, TRequest>(
+        this ActorSystem actorSystem,
+        List<IActorRef<TActor, TRequest>> instances
+    )
+        where TActor : IActor<TRequest> where TRequest : class, IConsistentHashable
+    {
+        return actorSystem.Spawn<ConsistentHashActor<TActor, TRequest>, TRequest>(null, instances);
+    }
+
+    /// <summary>
+    /// Creates a Consistent Hash router specifying name and number of instances
+    /// </summary>
+    /// <typeparam name="TActor"></typeparam>
+    /// <typeparam name="TRequest"></typeparam>
+    /// <typeparam name="TResponse"></typeparam>
+    /// <param name="actorSystem"></param>
+    /// <param name="name"></param>
+    /// <param name="instances"></param>
+    /// <returns></returns>
+    public static IActorRef<ConsistentHashActor<TActor, TRequest, TResponse>, TRequest, TResponse>
+        CreateConsistentHashRouter<TActor, TRequest, TResponse>(
+            this ActorSystem actorSystem,
+            string name,
+            int instances
+        )
+        where TActor : IActor<TRequest, TResponse> where TRequest : class, IConsistentHashable where TResponse : class
+    {
+        return actorSystem.Spawn<ConsistentHashActor<TActor, TRequest, TResponse>, TRequest, TResponse>(name, instances);
+    }
+
+    /// <summary>
+    /// Creates a Consistent Hash router specifying name and number of instances
+    /// </summary>
+    /// <typeparam name="TActor"></typeparam>
+    /// <typeparam name="TRequest"></typeparam>
+    /// <typeparam name="TResponse"></typeparam>
+    /// <param name="actorSystem"></param>
+    /// <param name="instances"></param>
+    /// <returns></returns>
+    public static IActorRef<ConsistentHashActor<TActor, TRequest, TResponse>, TRequest, TResponse>
+        CreateConsistentHashRouter<TActor, TRequest, TResponse>(
+            this ActorSystem actorSystem,
+            int instances
+        )
+        where TActor : IActor<TRequest, TResponse> where TRequest : class, IConsistentHashable where TResponse : class
+    {
+        return actorSystem.Spawn<ConsistentHashActor<TActor, TRequest, TResponse>, TRequest, TResponse>(null, instances);
+    }
+
+    /// <summary>
+    /// Creates a Consistent Hash router specifying name and a list of instances
+    /// </summary>
+    /// <typeparam name="TActor"></typeparam>
+    /// <typeparam name="TRequest"></typeparam>
+    /// <typeparam name="TResponse"></typeparam>
+    /// <param name="actorSystem"></param>
+    /// <param name="name"></param>
+    /// <param name="instances"></param>
+    /// <returns></returns>
+    public static IActorRef<ConsistentHashActor<TActor, TRequest, TResponse>, TRequest, TResponse>
+        CreateConsistentHashRouter<TActor, TRequest, TResponse>(
+            this ActorSystem actorSystem,
+            string name,
+            List<IActorRef<TActor, TRequest, TResponse>> instances
+        )
+        where TActor : IActor<TRequest, TResponse> where TRequest : class, IConsistentHashable where TResponse : class
+    {
+        return actorSystem.Spawn<ConsistentHashActor<TActor, TRequest, TResponse>, TRequest, TResponse>(name, instances);
+    }
+
+    /// <summary>
+    /// Creates a Consistent Hash router specifying name and a list of instances
+    /// </summary>
+    /// <typeparam name="TActor"></typeparam>
+    /// <typeparam name="TRequest"></typeparam>
+    /// <typeparam name="TResponse"></typeparam>
+    /// <param name="actorSystem"></param>
+    /// <param name="instances"></param>
+    /// <returns></returns>
+    public static IActorRef<ConsistentHashActor<TActor, TRequest, TResponse>, TRequest, TResponse>
+        CreateConsistentHashRouter<TActor, TRequest, TResponse>(
+            this ActorSystem actorSystem,
+            List<IActorRef<TActor, TRequest, TResponse>> instances
+        )
+        where TActor : IActor<TRequest, TResponse> where TRequest : class, IConsistentHashable where TResponse : class
+    {
+        return actorSystem.Spawn<ConsistentHashActor<TActor, TRequest, TResponse>, TRequest, TResponse>(null, instances);
+    }
+}
